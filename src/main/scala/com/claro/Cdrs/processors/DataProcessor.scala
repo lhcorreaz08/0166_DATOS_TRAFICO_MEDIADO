875fc82678d743adef07ca619d8910eabf49805a
lhcorreaz08 <53351667+lhcorreaz08@users.noreply.github.com>
2022-01-28 15:12:13 -0500

    Changue DataProcessor.scala to Production Data Table Comment Test Table

--- src/main/scala/com/claro/Cdrs/processors/DataProcessor.scala
+++ src/main/scala/com/claro/Cdrs/processors/DataProcessor.scala
@@ -30,8 +30,9 @@ class DataProcessor(
 
 
 
-  override def getTrafficTable: String = "datos.tbl_tmp_fact_datos_trafico_tmp"
+  //override def getTrafficTable: String = "datos.tbl_tmp_fact_datos_trafico_tmp"
 
+  override def getTrafficTable: String = "datos.tbl_fact_datos_trafico"
 
 
   override def getSchema: StructType = Entities.dataSchema
